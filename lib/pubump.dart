467be59026a10d62cdefb144a944044cdd0660a9
Lynn <lynn.van.hauwe@gmail.com>
2019-11-15 04:03:04 +0100

    0.1.6: Add simple error handling

--- lib/pubump.dart
+++ lib/pubump.dart
@@ -3,9 +3,30 @@ import './level.dart';
 import './version.dart';
 export './level.dart';
 
+/// Print an error message to STDERR and exit with exit code 1.
+void die(String message) {
+  stderr.write('$message\n');
+  exit(1);
+}
+
+/// Expect a ProcessResult to be successful, aborting if it isn't.
+ProcessResult successfully(ProcessResult result) {
+  if (result.exitCode != 0) {
+    die(result.stderr);
+  }
+  return result;
+}
+
+/// Update the working directory's pubspec by the given level.
 Future<Version> updatePubspec(Level level) async {
-  final pubspec = await File('pubspec.yaml');
-  final lines = await pubspec.readAsLines();
+  var pubspec;
+  var lines;
+  try {
+    pubspec = await File('pubspec.yaml');
+    lines = await pubspec.readAsLines();
+  } catch (e) {
+    die("Error: Couldn't find pubspec.yaml. Run pubump from your package root.");
+  }
   final regExp = RegExp(r'(version:\s+)(\d+)\.(\d+)\.(\d+)(?:\+(\d+))?$');
   List<String> updatedLines = [];
   Version version;
@@ -30,25 +51,10 @@ Future<Version> updatePubspec(Level level) async {
   return version;
 }
 
-Future<void> updateChangelog(Version version, String message) async {
-  final changelog = await File('CHANGELOG.MD');
-  final content = await changelog.readAsString();
-  await changelog.writeAsString('## $version\n\n- $message\n\n$content');
-}
-
-Future<bool> anythingUnstaged() async {
-  final result = await Process.run('git', ['diff', '--exit-code']);
-  return result.exitCode == 1;
-}
-
+/// Return `true` if no files are staged in git.
 Future<bool> nothingStaged() async {
   final result = await Process.run('git', ['diff', '--cached', '--exit-code']);
   return result.exitCode == 0;
-}
-
-void die(String message) {
-  stderr.write('$message\n');
-  exit(1);
 }
 
 Future<String> coloredDiff() async {
@@ -57,36 +63,43 @@ Future<String> coloredDiff() async {
   return result.stdout;
 }
 
-ProcessResult successfully(ProcessResult result) {
-  if (result.exitCode != 0) {
-    stderr.write(result.stderr);
-    exit(1);
-  }
-  return result;
-}
-
 Future<void> pubump({bool push, bool publish, Level level, String message}) async {
-  // if (await anythingUnstaged()) {
-  //   die('You have unstaged changes; please stash them.');
-  // }
+  // Don't allow an empty version bump.
   if (await nothingStaged()) {
-    die('Please stage the changes (git add ...) you would like pubump to commit.');
+    die('Error: Please stage the changes (git add ...) you would like pubump to commit.');
   }
-  final version = await updatePubspec(level);
-  await updateChangelog(version, message);
 
+  // Update files.
+  final version = await updatePubspec(level);
+  if (version == null) {
+    die("Error parsing pubspec.yaml: Didn't find a `version: 1.2.3+4` line to bump.");
+  }
+  var changelog, content;
+  try {
+    changelog = await File('CHANGELOG.md');
+    content = await changelog.readAsString();
+  } catch (e) {
+    die("Error: Couldn't find CHANGELOG.md. Run pubump from your package root, and create a CHANGELOG.md file there.");
+  }
+  await changelog.writeAsString('## $version\n\n- $message\n\n$content');
+
+  // Create a commit.
   final commit = successfully(await Process.run('git', ['commit', '-am', '$version: $message']));
-  print(commit.stdout);
-  // print(await coloredDiff());
+  print(commit.stdout); // print(await coloredDiff());
+
+  // Optional post-bump actions.
   if (push) {
+    print('Running `git push`...');
     successfully(await Process.run('git', ['push']));
   }
   if (publish) {
+    print('Running `pub publish --force`...');
     successfully(await Process.run('pub', ['publish', '--force']));
   }
+
+  // Report success.
   final verbed =
       push ? (publish ? 'Pushed and published' : 'Pushed') : (publish ? 'Published' : 'Created');
-  print('$verbed version $version');
-
+  print('All done! $verbed version $version.');
   exit(0);
 }
